eea22947576b4a3fb0c2ecf790553765af999fd8
RoccoRiccardi <Rocco.Riccardi.42@gmail.com>
2015-11-10 09:51:25 -0500

    added Models project and models for Movie, Note, Borrower, and Rating

---
+++ DVDLibrary/DVDLibraryModels/Note.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVDLibraryModels
+{
+    public class Note
+    {
+        public int NoteID { get; set; }
+        public DateTime TimeAdded { get; set; }
+        public string Content { get; set; }
+    }
+}
