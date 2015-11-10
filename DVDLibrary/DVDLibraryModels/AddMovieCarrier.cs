e1d01b4f8054d151b6b39ad079d32a9b7e248037
Andrew Tang <atang.business@gmail.com>
2015-11-10 16:05:34 -0500

    added AddMovieCarrier for addmovie binding

---
+++ DVDLibrary/DVDLibraryModels/AddMovieCarrier.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVDLibraryModels
+{
+    public class AddMovieCarrier
+    {
+        public string Title { get; set; }
+        public string DateReleased { get; set; }
+        public string MPAARatingID { get; set; }
+        public List<string> DirectorID { get; set; }
+        public string StudioID { get; set; }
+        public List<string> ActorID { get; set; } 
+        public string Synopsis { get; set; }
+        public string OwnerRatingID { get; set; }
+        public string Copies { get; set; }
+        public string ImageURL { get; set; }
+    }
+}
