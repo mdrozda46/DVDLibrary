e6cdf8af4fbfa1aac478dfd1ad6e6535c9b002c7
Mike Drozda <mdrozda@gmail.com>
2015-11-12 13:28:45 -0500

    Setup initial unit tests

---
+++ DVDLibrary/DVDLibraryTests/RepositoryTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DVDLibraryData.Repository;
+using NUnit.Framework;
+
+namespace DVDLibraryTests
+{
+    [TestFixture]
+    public class RepositoryTests
+    {
+
+        [Test]
+        public void GetMPAARatings()
+        {
+            var repo = new DVDLibraryADO();
+            var MPAARatings = repo.GetMPAARatings();
+
+            Assert.AreEqual(MPAARatings.Count, 5);
+            Assert.AreEqual(MPAARatings[0].MPAARating, "G");
+
+        }
+    }
+}
