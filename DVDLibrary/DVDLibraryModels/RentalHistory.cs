a1bf78e96233ec55587e7d4ed9c4801fd5fc91d9
Andrew Tang <atang.business@gmail.com>
2015-11-13 15:08:13 -0500

    fixed nullable date types

--- DVDLibrary/DVDLibraryModels/RentalHistory.cs
+++ DVDLibrary/DVDLibraryModels/RentalHistory.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace DVDLibraryModels
 {
@@ -10,8 +11,13 @@ namespace DVDLibraryModels
     {
         public string Name { get; set; }
         public string Title { get; set; }
+
+        //[DisplayFormat(DataFormatString="{0:MM/dd/yyy}")]
         public DateTime DateBorrowed { get; set; }
+
+        //[DisplayFormat(DataFormatString = "{0:MM/dd/yyy}")]
         public DateTime DateReturned { get; set; }
+
         public int  SerialNumberID { get; set; }
     }
 }
