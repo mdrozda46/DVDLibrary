aac4199d358abf8f5d9c9ba2d7bd538fc55b0463
Andrew Tang <atang.business@gmail.com>
2015-11-17 09:01:35 -0500

    added multiselect, added search bars, made theme white

---
+++ DVDLibrary/DVDLibraryMVC/Scripts/app/SearchBar.js
@@ -0,0 +1,5 @@
+﻿$(document).ready(function() {
+    $('#search-nodata').hideseek({
+        nodata: 'No results found'
+    });
+});
---
+++ DVDLibrary/DVDLibraryMVC/Scripts/app/Select2.js
@@ -0,0 +1,4 @@
+﻿$(document).ready(function () {
+   
+    $('.mselect').select2();
+});
